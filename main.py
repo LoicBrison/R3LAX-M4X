ab610338199649460b7b6b4452243bd8bff1f8ee
Clément Tinarelli <clement.tinarelli@gmail.com>
2022-10-11 15:37:10 +0200

    [Add] Fullscreen that can be toogle with debug param and a cool new feature :).

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ SANS_SPRITE_RECT = (0, 0, 36, 33)
 SANS_TEXT_BOX = (0, SCREEN_SIZE[1]-(SCREEN_SIZE[1]/4), SCREEN_SIZE[0], SCREEN_SIZE[1]/4)
 
 CHOICES = [
-	'Tripotanus',
-	'Lorem ipsum',
-	'TentaCULe',
-	'Tabouret',
-	'TentaCULe',
-	'Tabouret',
-	'a',
-	'b',
-	'c',
-	'd',
+	"Nani bakayaro ?!!!",
+	"Salut toi !",
+	"Qui est la ?",
+	"Passe un message",
+	"A plus !",
 ]
+
+INITIAL_PROMPT = "Que veux tu faire ?"
 
 if __name__ == '__main__':
 	pygame.init()
-	surface = pygame.display.set_mode(SCREEN_SIZE, NOFRAME)
+
+	flags = pygame.NOFRAME | pygame.FULLSCREEN
+	if len(sys.argv) > 1 and sys.argv[1].lower() == 'debug': flags &= ~pygame.FULLSCREEN
+	surface = pygame.display.set_mode(SCREEN_SIZE, flags)
 	pygame.display.set_caption("")
 	doit = True
 	textBox = pygame.Rect(SANS_TEXT_BOX[0]+X_MARGIN, SANS_TEXT_BOX[1]+Y_MARGIN, SANS_TEXT_BOX[2]-(Y_MARGIN*2), SANS_TEXT_BOX[3]-(Y_MARGIN*2))
@@ -47,7 +47,8 @@ if __name__ == '__main__':
 	font = pygame.font.Font('undertale.ttf', 18)
 
 	choices = [font.render(c, True, (255, 255, 255)) for c in CHOICES]
-	textSans = 'Que veux tu faire ?'
+
+	textSans = INITIAL_PROMPT
 	spriteCount = int(spritesheet.get_width()/SANS_SPRITE_RECT[2])
 	t0 = time.time_ns()
 	textTimer = 0
@@ -65,14 +66,15 @@ if __name__ == '__main__':
 	fittingCount = int(math.floor((surface.get_height()-((surface.get_height()-textBox.y)+choicesBaseY))/(height+choicesBaseY)))
 	choicesRect = pygame.Rect(arrowX, choicesBaseY, surface.get_width()-(arrowX*2), surface.get_height()-((surface.get_height()-textBox.y)+choicesBaseY*2))
 	arrowBottomX = (surface.get_width() - (arrowX * 2))-(arrowBottom.get_width()/2)
-
+	choicesHitBox = [pygame.Rect(textX, choicesBaseY+(textOffsetY*i), c.get_width(), c.get_height()) for i, c in enumerate(choices)]
 
 	currentSelectedText = 0
 
 	shownChoices = min(len(CHOICES), fittingCount)
 
 	arrowTimer = 400.0
-
+	doAction = 0
+	actionTimer = 0.0
 	while doit:
 		t1 = time.time_ns()
 		dt = float(t1 - t0)/1000000.0
@@ -81,16 +83,33 @@ if __name__ == '__main__':
 		sansTimer += dt
 		arrowTimer += dt
 
+
+		if doAction == 2: actionTimer += dt
+
+		if actionTimer > 1000.0:
+			if currentSelectedText == 2:
+				textSans = INITIAL_PROMPT
+				textSansIndex = 0
+			elif currentSelectedText == 4:
+				doit = False
+			doAction = 0
+			spriteIndex = 0
+			actionTimer = 0
+
 		if textTimer > 100.0:
 			textTimer -= 100.0
-			
 			textSansIndex = min(textSansIndex+1, len(textSans))
+			while len(textSans) > textSansIndex and textSans[textSansIndex].isspace():
+				textSansIndex = min(textSansIndex + 1, len(textSans))
 
-		if sansTimer > 1000.0:
+		if textSansIndex == len(textSans) and doAction == 1: doAction = 2
+
+
+		if sansTimer > 1000.0 and doAction == 0:
 			sansTimer -= 1000.0
 			spriteIndex += 1
 			spriteIndex %= spriteCount
-		if arrowTimer > 1200.0:
+		if arrowTimer > 1200.0 and doAction == 0:
 			arrowTimer -= 1200.0
 
 		textSansImg = font.render(textSans[0:textSansIndex], True, (255, 255, 255))
@@ -112,60 +131,33 @@ if __name__ == '__main__':
 		if len(CHOICES) > fittingCount:
 			surface.blit(arrowBottom, (((surface.get_width() - arrowBottom.get_width()) / 2, choiceY)))
 
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				doit = False
-			if event.type == KEYDOWN:
-				if event.key == pygame.K_UP:
-					currentSelectedText -= 1
+		if doAction == 0:
+			for event in pygame.event.get():
+				if event.type == pygame.MOUSEBUTTONUP:
+					for i, hitbox in enumerate(choicesHitBox):
+						if event.pos[0] >= hitbox.x and event.pos[1] >= hitbox.y and event.pos[0] <= hitbox.x+hitbox.width and event.pos[1] <= hitbox.y+hitbox.height:
+							currentSelectedText = i
+							break
+					print(event)
+				if event.type == QUIT:
+					doit = False
+				if event.type == KEYDOWN:
+					if event.key == pygame.K_UP:
+						currentSelectedText -= 1
+					if event.key == pygame.K_DOWN:
+						currentSelectedText += 1
 					currentSelectedText %= shownChoices
-				if event.key == pygame.K_DOWN:
-					currentSelectedText += 1
-				currentSelectedText %= shownChoices
-
+					if event.key == pygame.K_RETURN:
+						if currentSelectedText == 2:
+							doAction = 1
+							spriteIndex = 2
+							textSans = 'Ta grosse daronne :).'
+							textSansIndex = 0
+						if currentSelectedText == 4:
+							doAction = 1
+							spriteIndex = spriteCount-1
+							textSans = 'A plus le reuf !!'
+							textSansIndex = 0
 		pygame.display.update()
 	pygame.display.quit()
 	sys.exit()
-
-def showMainMenu(error):
-	os.system('cls')
-	if error:
-		print("Nani bakayaro ?!!!")
-	print("1 - Salut toi !")
-	print("2 - Qui est là ?")
-	print("3 - Passe un message")
-	print("4 - A plus !")
-
-def communicate():
-	pass
-
-def whosThere():
-	pass
-
-def sendMessage():
-	pass
-
-def main():
-	doit = True
-	error = False
-	while doit:
-		showMainMenu(error)
-		error = False	
-		userInput = input()
-		try:
-			n = int(userInput)
-			if n == 1:
-				communicate()
-			elif n == 2:
-				whosThere()
-			elif n == 3:
-				sendMessage()
-			elif n == 4:
-				doit = False
-			else:
-				error = True
-		except ValueError as e:
-			pass
-
-if __name__ == '__main__':
-	pass
